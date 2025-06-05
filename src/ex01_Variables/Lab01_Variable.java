a03304e4b1be388e4546df36dbf2291243518bca
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-06-05 22:54:53 +0530

    05th June 2025

--- src/ex01_Variables/Lab01_Variable.java
+++ src/ex01_Variables/Lab01_Variable.java
@@ -1,4 +1,14 @@
 package ex01_Variables;
 
 public class Lab01_Variable {
+    public static void main(String[] args) {
+
+        System.out.println("Sai");
+
+        int age1 = 26;
+        int age2 = 66;
+        System.out.println(age1);
+        System.out.println(age2);
+
+    }
 }
--- src/ex01_Variables/Lab02_Varible02.java
+++ src/ex01_Variables/Lab02_Varible02.java
@@ -1,4 +1,13 @@
 package ex01_Variables;
 
 public class Lab02_Varible02 {
+    public static void main(String[] args) {
+        int age = 65;
+        // int -> data type = - whole numbers (not decimals)
+        // age ->  variable_name / identifier
+        // = -> assignment operator
+        // 65 -> literal / variable_value
+
+
+    }
 }

9baeb675e24d0a34bffd59c5f110c5c1b3657eea
Sai-kumaRRR <alamurisaikumar03@gmail.com>
2025-06-24 23:06:32 +0530

    24th June 2025  practice

---
+++ src/ex10_Loop/While03.java
@@ -0,0 +1,13 @@
+package ex10_Loop;
+
+public class While03 {
+    public static void main(String[] args) {
+        int a = 11;
+        while (a < 11) {
+            System.out.println(a);
+
+            a++;
+        }
+        System.out.println(a);
+    }
+}
---
+++ src/ex10_Loop/While04.java
@@ -0,0 +1,12 @@
+package ex10_Loop;
+
+public class While04 {
+    public static void main(String[] args) {
+
+        int i = 0;
+        while (i < 10 ){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
